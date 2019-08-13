603ea83d73ff0e1231884b6d22b1202417c307e1
ganeshvenkatesh1999 <54060011+ganeshvenkatesh1999@users.noreply.github.com>
2019-08-13 14:47:34 +0530

    Add files via upload

---
+++ alp or con.py
@@ -0,0 +1,14 @@
+
+
+
+Alp=(input())
+
+if Alp=="a" or Alp=="e" or Alp=="i" or Alp=="o" or Alp=="u" :
+    print("Vowel")
+elif Alp=="b" or Alp=="c" or Alp=="d" or Alp=="f" or Alp=="g" or Alp=="h" or Alp=="j" or Alp=="k" or Alp=="l" or Alp=="m" or Alp=="n" or Alp=="p" or Alp=="q" or Alp=="r" or Alp=="s" or Alp=="t" or Alp=="v" or Alp=="w" or Alp=="x" or Alp=="y" or Alp=="g": 
+    print("Consonant")
+else :
+    print("invaild")
+
+ 
+
